d26395bab86115ae89fe93064cc5c91c3bc58153
bione888 <gabrielcbione@gmail.com>
2024-01-12 19:39:57 -0300

    main

--- yourpf/pf.js
+++ yourpf/pf.js
@@ -1,8 +1,11 @@
-
 let tudo = document.querySelector('.td')
 let body = document.querySelector('body')
 let inputs = document.querySelector('input')
 let logins = document.querySelector('.login')
+let pword = document.querySelector('#pword')
+let unn = document.querySelector('#uname')
+let ppw = document.querySelector('#pw')
+    let password = ppw.value
 
 function login() {
      tudo.style.display = 'none'
@@ -12,12 +15,26 @@ function logout() {
     tudo.style.display = ''
     logins.style.display = 'none'
     let un = document.querySelector('#user')
-    let username = un.value
-    let passw = document.querySelector('#pw')
-    let pw = passw.value
-    let hello = document.querySelector('#uname')
-    let pass = document.querySelector('#pword')
+    let usernames = un.value
+    let passwordd = ppw.value
+    unn.innerHTML = `Username: ${usernames}`
+    pword.innerHTML = `Password : ${passwordd}`
+    pword.style.color = 'white'
+    pword.style.display = 'none'
+    unn.style.color = 'white'
+    unn.style.display = 'none'
+}
 
-    hello.innerHTML = `Ola , ${username}`
-    pass.innerHTML = `Senha : ${pw}`
+function ancora() {
+         let pwck = window.prompt('Confirm Password:')
+         let pwc = pwck.value
+         let password2 = password.value
+         let hideb = document.querySelector('#show')
+         if (pwc == password2) {
+               unn.style.display = ''
+               pword.style.display = ''
+               hideb.style.display = 'none'
+         } else {
+            window.alert('Senha Incorreta')
+         }
 }
